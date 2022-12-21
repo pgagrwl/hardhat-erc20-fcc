363e6b95cd8b757a24fb35c69d98b1c4c0f455e9
pgagrwl <pgagrwl@gmail.com>
2022-12-22 02:58:34 +0530

    Initial commit

--- contracts/ManualToken.sol
+++ contracts/ManualToken.sol
@@ -2,3 +2,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+contract ManualToken{
+    uint256 initialSupply;
+
+    mapping(address => uint256)public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    function transfer(address from, address to , uint256 amount) public{
+        balanceOf[from] -= amount;
+        balanceOf[to] +=amount;
+    }
+
+function transferFrom(address _from, address _to, uint256 _value) public returns(bool success){
+    require(_value <= allowance[_from][msg.sender]);
+    allowance[_from][msg.sender] -= _value;
+    transfer(_from, _to, _value);
+    return true;
+}
+
+}
